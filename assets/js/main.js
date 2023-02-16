5fe6f394e78dfc34fa04f11917bce4d55019cc61
ClaudioSUPERCODE <123948180+ClaudioSUPERCODE@users.noreply.github.com>
2023-02-16 17:32:07 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+let a = 12;
+console.log (a)
+
+let b = a * 3;
+console.log (b);
+
+document.write (b)
+
+console.log ("Hello World")
+
+console.log ("anton")
+
+let firstName = ("Anton")
+
+let age = 28
+console.log (age)
+
+console.log ("Trainer")
+
+let Job = "Trainer"
+console.log (Job)
+
+let married = "true"
+console.log (married)
+
+console.log (firstName + "ist" + age + "Jahre alt, von Beruf" + Job + "und ist verheiratet:" + married)
